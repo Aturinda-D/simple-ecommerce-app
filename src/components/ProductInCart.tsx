eedc165d6c3553af417a8dc90b04f941b559f8ae
deidave <atudav15@gmail.com>
2025-05-20 13:57:36 +0300

    Feat: Products component for the cart page

--- src/components/ProductInCart.tsx
+++ src/components/ProductInCart.tsx
@@ -5,7 +5,18 @@ import QuantityInput from "./QuantityInput";
 const ProductInCart: React.FC<productType> = ({ ...props }) => {
   return (
     <div className="min-h-30 flex justify-between items-center bg-gray-100 shadow">
-      <div>product details</div>
+      <div className="h-full w-fit flex">
+        <img className="w-30 h-30" src={props.image} alt="" />
+        <div className="ml-3 p-3 flex flex-col gap-1 justify-center items-start">
+          <h2 className="text-xl text-black font-semibold">{props.title}</h2>
+          <h4 className="text-[16px] text-black font-normal">
+            Price: ${props.price}
+          </h4>
+          <button className="text-[var(--accent-1)] font-medium cursor-pointer hover:underline">
+            Remove
+          </button>
+        </div>
+      </div>
       <div className="w-2/5 flex justify-between items-center">
         <QuantityInput />
         <h4 className="pr-4 text-black text-lg font-semibold">
