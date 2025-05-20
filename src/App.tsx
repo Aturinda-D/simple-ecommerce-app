3beb169024b4ea99fbe4c12a3912d710689cb2ad
deidave <atudav15@gmail.com>
2025-05-20 11:11:26 +0300

    Project config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Products from "./pages/Products";
+import RootLayout from "./layouts/RootLayout";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Products />} />
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="cart" element={<Cart />} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,5 @@
+const NavBar = () => {
+  return <div className="h-20 w-full bg-[var(--accent-1)]">Navigation Bar</div>;
+};
+
+export default NavBar;
