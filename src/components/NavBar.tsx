83777d9f91651250a80011da73112fe77b16ed1a
deidave <atudav15@gmail.com>
2025-05-20 11:43:03 +0300

    Feat: Added Navbar links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,43 @@
-const NavBar = () => {
-  return <div className="h-20 w-full bg-[var(--accent-1)]">Navigation Bar</div>;
+import { NavLink } from "react-router-dom";
+
+type routeTypes = {
+  route: string;
+  page: string;
 };
+const routes: routeTypes[] = [
+  {
+    route: "/",
+    page: "home",
+  },
+  {
+    route: "products",
+    page: "products",
+  },
+  {
+    route: "cart",
+    page: "cart",
+  },
+];
+
+const NavBar = () => {
+  return (
+    <nav className="h-16 w-full">
+      <ul className="h-full w-full flex justify-center items-center gap-7 font-bold text-xl capitalize bg-white *:hover:underline *:hover:text-[var(--accent-1)]">
+        {routes?.map((route) => (
+          <NavLink
+            key={route.page}
+            to={route?.route}
+            className={({ isActive }) =>
+              isActive ? "text-[var(--accent-1)]" : ""
+            }
+          >
+            {route?.page}
+          </NavLink>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+// Maybe use amber-600
 
 export default NavBar;
