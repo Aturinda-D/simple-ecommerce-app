de1ada2dd43612a4bff76f204fae3ee832df96b0
BrunoDavie <kiisabruno04@gmail.com>
2025-05-20 12:04:31 +0300

    first commit

---
+++ src/components/Product.tsx
@@ -0,0 +1,17 @@
+// type productDetails = {
+//   id: string;
+//   image: string;
+//   title: string;
+// };
+
+const Product = () => {
+  return (
+    <div className="grid grid-cols-1">
+      <img src="/assets/images/bag_1.jpg" alt="" />
+      <h4 className=" --accent-1">image title</h4>
+      <p className=" --accent-1">price</p>
+    </div>
+  );
+};
+
+export default Product;
