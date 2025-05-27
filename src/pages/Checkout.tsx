fbab7e4ca58b16257cda870116108c6e4b813805
deidave <atudav15@gmail.com>
2025-05-27 17:23:56 +0300

    Final commit

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,13 +1,13 @@
 const Checkout = () => {
   return (
     <div className="h-[100vh] p-20  justify-center items-end bg-[url('/assets/images/backgroud-image.png')] bg-no-repeat bg-cover bg-center">
-      <h1 className="text-4xl ">
-        Thanks for supporting us our dear customer , meet you at your next order{" "}
+      <h1 className="text-4xl text-white  mb-5">
+        Thanks for supporting us dear customer, meet you at your next order{" "}
       </h1>
       <p>
         <a
           href={"/"}
-          className="p-3 ml-auto m-5 w-fit inline-flex justify-center items-center gap-3 text-[purple] bg-[white] font-semibold rounded-full hover:cursor-pointer hover:opacity-90"
+          className="py-3 px-6 ml-auto m-5 w-fit inline-flex justify-center items-center gap-3 text-[purple] bg-[white] font-semibold rounded-full hover:cursor-pointer hover:opacity-90"
         >
           Go Back Home
         </a>
